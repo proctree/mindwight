df1e84da764e8bb438f92df4a24976f76fdb67c9
proctree <proctree@gmail.com>
2014-01-26 03:46:31 +0100

    added the --add option

--- bin/mindwight.pl
+++ bin/mindwight.pl
@@ -12,13 +12,13 @@ use Getopt::Long;
 sub usage(){
 	#these options are pretty lazely done and described
 	#I'll change the descriptions later
-	print "Usage: perl $0 [--list|--remove name|--search search_term|--add <-n name -s salt -p password>|--get -n name -s salt]\n";
+	print "Usage: perl $0 [--list|--remove name|--search search_term|--add <-n name -k key -p password>|--get -n name -k key]\n";
 	print "--list or -l: lists all known passwords by their name\n";
 	print "--remove or -r: removes a password from the password safe\n";
 	print "--search or -s: searches for a password by it's name\n";
 	print "--add or -a: adds a new password to the safe\n";
 	print " 	--name or -n: the name of the password(eg. \"yahoo acc\")\n";
-	print "		--salt or -s: the salt used for the encryption of the password\n";
+	print "		--key or -k: the key used for the encryption of the password\n";
 	print "		--password or -p: the password itself\n";
 	print "--get or -g: gets the password itself\n";
 }
@@ -26,9 +26,23 @@ sub usage(){
 my @auxarg=@ARGV; #i copy the argv array into another array so i can still have access to it
 		  #after getoptions is done with it
 my $arghelp = ''; #-h or --help
-GetOptions('h|help' => \$arghelp);
+my $argadd = ''; #-a or --add
+my $argname = ''; #-n or --name
+my $argkey = ''; #-k or --key
+my $argpassword = ''; #-p or --password
 
-if($arghelp eq 1 or @auxarg==0){
+GetOptions('help' => \$arghelp,
+	   'add' => \$argadd,
+	   'name:s' => \$argname,
+	   'key:s' => \$argkey,
+	   'password:s' => \$argpassword
+);
+
+if($arghelp eq 1 or @auxarg==0){ #--help
 	usage();
 	exit;
+}
+if($argadd eq 1){ #--add
+	die "Not enough arguments for mode --add, stopped" unless $argname ne '' and $argkey ne '' and $argpassword ne ''; #check that all of the arguments are specified
+	mindAdd($argname,$argkey,$argpassword); #mindAdd is in Mind::Wight::Crypt
 }
--- lib/Mind/Wight/Crypt.pm
+++ lib/Mind/Wight/Crypt.pm
@@ -2,8 +2,25 @@
 use warnings;
 use strict;
 
+use File::Basename;
+use Cwd;
+
+use lib dirname(dirname(cwd()));
+use Crypt::Tea;
 use Exporter qw(import);
 
-our @EXPORT_OK = qw();
+our @EXPORT_OK = qw(mindAdd);
+
+sub mindAdd{
+	die "Not enough arguments, stopped" unless @_>2;
+	die "Too many arguments, stopped" unless @_<4;
+	my ($name,$key,$pass) = @_;
+	if(not(-e (cwd().'/db.mwc'))){
+		system('touch '.cwd().'/db.mwc');
+	}
+	open my $mfo,">>",(cwd().'/db.mwc');
+	print $mfo "$name\n".encrypt($pass,$key)."\n";
+	close $mfo;
+}
 
 1;
